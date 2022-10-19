cde5ea7621cc913aa2a54c3f54e12c84a29adba9
Jameswong <wcf0123456789@gmail.com>
2022-10-19 10:21:18 +0800

    1. Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from pages import auth
-from api.pyneer import db
+
 
 if __name__ == "__main__":
-    # auth.auth_page()
-
-    a = db.delete_one(collection="asd", select={"username": "admin"})
-
-    print(a)
+    auth.auth_page()
