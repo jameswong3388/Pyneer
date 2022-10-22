f0eb5b4cf82924bcd73579067bc72063e2207e3e
Jameswong <wcf0123456789@gmail.com>
2022-10-22 09:50:55 +0800

    1. Update schema.py documentation

--- api/pyneer/db/schema.py
+++ api/pyneer/db/schema.py
@@ -1,5 +1,5 @@
 """
-Schema is used to validate the data that is being passed to the database.
+Schema is used to validate the data that is being passed to the database
 """
 
 
@@ -7,6 +7,28 @@ class schema:
     def __init__(self, schemas):
         self.schema = schemas
 
+    """
+    Validate the data that is being passed to the database
+    
+    e.g 
+    from api.pyneer.db.schema import schema
+
+    model_schema = {
+        "name": str,
+        "age": int,
+        "email": str,
+        "password": int,
+    }
+
+    datas = {
+        "name": 'John',
+        "age": 20,
+        "email": "email@localhost.com",
+        "password": '1f'
+    }
+    
+    print(schema(model_schema).validate(datas))
+    """
     def validate(self, data):
         for key, value in self.schema.items():
             if key not in data:
