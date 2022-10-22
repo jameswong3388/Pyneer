d718cab75d763c456d87f4627de46c106bb1ce40
Jameswong <wcf0123456789@gmail.com>
2022-10-22 09:49:00 +0800

    1. Added beta schema.py to validate data

--- api/pyneer/db/schema.py
+++ api/pyneer/db/schema.py
@@ -1,3 +1,16 @@
 """
 Schema is used to validate the data that is being passed to the database.
 """
+
+
+class schema:
+    def __init__(self, schemas):
+        self.schema = schemas
+
+    def validate(self, data):
+        for key, value in self.schema.items():
+            if key not in data:
+                return {'valid': False, 'message': f'{key} is required'}
+            if type(data[key]) != value:
+                return {'valid': False, 'message': f'{key} must be {value}'}
+        return {'valid': True}
